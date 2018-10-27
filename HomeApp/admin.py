4ab618fb1ff166ec28344d76e2814ff6c35abf04
qiuzhe <2810201146@qq.com>
2018-10-27 13:09:48 +0800

    1
    1

--- HomeApp/admin.py
+++ HomeApp/admin.py
@@ -6,6 +6,10 @@ from .models import APPLIST,dynamic
 @admin.register(APPLIST)
 class APPLISTeleAdmin(admin.ModelAdmin):
     list_display = ('id','title','msg','PANdomurl','WANdomurl','star_time','last_time','isDelete')
+    #筛选器
+    list_filter =('isDelete', 'title', 'msg', 'PANdomurl') #过滤器
+    search_fields =('id', 'title', 'msg') #搜索字段
+    date_hierarchy = 'star_time'    # 详细时间分层筛选　
 
 @admin.register(dynamic)
 class dynamicTeleAdmin(admin.ModelAdmin):
