235d6f69567b9d1391dbd37b91fc1d4b5788a6f4
qiuzhe <2810201146@qq.com>
2018-11-02 16:59:07 +0800

    0
    1

---
+++ Start.py
@@ -0,0 +1,6 @@
+import os
+# os.system("python ./2.py")
+# os.system("python ./4.py")
+result = os.popen("python manage.py runserver 127.0.0.1:80")
+# print (result.read())
+# os.system("python ./sunny.py")
